1a96c65ea689256a2a43d12330d64c3ff212bf3c
ostafiichuko <issimo333@gmail.com>
2025-03-06 17:22:24 +0200

    feat: hw 2

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 import uvicorn
-from fastapi import FastAPI, Depends, HTTPException, Request, status
-from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy import text
@@ -39,8 +39,11 @@ def healthchecker(db: Session = Depends(get_db)):
         return {"message": "Welcome to FastAPI!"}
     except Exception as e:
         print(e)
-        raise HTTPException(status_code=500, detail="Error connecting to the database")
+        raise HTTPException(
+            status_code=500, detail="Error connecting to the database")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), log_level="info", reload=True)
+    uvicorn.run(
+        "main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)),
+        log_level="info", reload=True)
